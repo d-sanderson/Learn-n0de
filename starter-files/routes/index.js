40784970386e25ff7cfe191a1eefaf719eff90aa
dsanderson90 <dsanderson3@cnm.edu>
2019-03-23 14:29:27 -0600

    set up mongodb atlas and started learning about express routes

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const david = { name: 'David', age: '28', cool: true };
+  // res.send('Hey! It works! David is the best :) ');
+  // res.json(david);
+  res.send(req.query);
 });
 
+router.get('/test', function(req, res) {
+  res.send('Hey! I work');
+});
 module.exports = router;

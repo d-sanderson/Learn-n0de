ba427ce9e7913c9ca05cf7f9a397783648d3c901
dsanderson90 <dsanderson3@cnm.edu>
2019-03-26 11:21:17 -0600

    moved code from route to storeController.js

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,24 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', (req, res) => {
-  // const david = { name: 'David', age: '28', cool: true };
-  // res.send('Hey! It works! David is the best :) ');
-  // res.json(david);
-  // res.send(req.query);
-  res.render('hello', { 
-    name:'David',
-    dog: req.query.dog
-  });
-});
+router.get('/', storeController.homePage)
 
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse)
-})
-router.get('/test', function(req, res) {
-  res.send('Hey! I work');
-});
 module.exports = router;

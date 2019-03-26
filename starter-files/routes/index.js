6e7b412a8aedf63dcd0a6fb7408ae4b29d2e6522
dsanderson90 <dsanderson3@cnm.edu>
2019-03-26 11:12:52 -0600

    learning about templating in pug and passing responses and requests.

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  const david = { name: 'David', age: '28', cool: true };
+  // const david = { name: 'David', age: '28', cool: true };
   // res.send('Hey! It works! David is the best :) ');
   // res.json(david);
-  res.send(req.query);
+  // res.send(req.query);
+  res.render('hello', { 
+    name:'David',
+    dog: req.query.dog
+  });
 });
 
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse)
+})
 router.get('/test', function(req, res) {
   res.send('Hey! I work');
 });

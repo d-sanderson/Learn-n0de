1a7b11f9b7bf997f6efc8355576cc1347f1e4570
dsanderson90 <dsanderson3@cnm.edu>
2019-05-07 11:58:20 -0600

    reviewing core concepts of Node and how to pass data using Pug

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
-// Do work here
-router.get('/', storeController.homePage)
+
+// Home page route
+//Pug is a popular html templating language that used to be called Jade. Its often used with Node
+
+router.get('/', (req,res) => {
+  res.render('hello', {
+    name: 'David',
+    dog: req.query.dog
+  });
+});
+
+
+// router.get('/', storeController.homePage)
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+res.send(reverse);
+});
 
 module.exports = router;

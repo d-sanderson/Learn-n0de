41e705fb908ccc2bfda95a688933eb35b6081761
dsanderson90 <dsanderson3@cnm.edu>
2019-05-07 12:17:59 -0600

    learned how to use controllers so that routes are independent from their views. very similar to laraval MVC

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,16 +3,10 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 
 // Home page route
-//Pug is a popular html templating language that used to be called Jade. Its often used with Node
-
-router.get('/', (req,res) => {
-  res.render('hello', {
-    name: 'David',
-  });
-});
+//Pug is a popular html templating language that used to be called Jade. Its often used by Node developers
 
 
-// router.get('/', storeController.homePage)
+router.get('/', storeController.homePage)
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');

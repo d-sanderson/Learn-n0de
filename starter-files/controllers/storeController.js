10dd25c7fc6b4498239e5c6d20d7df40ec9f6a7e
dsanderson90 <dsanderson3@cnm.edu>
2019-05-07 13:08:43 -0600

    created a reusable form using pug mixins and add store get and post routes with corresponding controller methods

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,14 @@
-
-
 exports.homePage = (req, res) => {
+  console.log(req.name);
+  console.log(req.message);
   res.render('index');
+};
+
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: '🏬 Add Store'});
+};
+
+exports.createStore = (req, res) => {
+  
 }
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -6,7 +6,9 @@ const storeController = require('../controllers/storeController')
 //Pug is a popular html templating language that used to be called Jade. Its often used by Node developers
 
 
-router.get('/', storeController.homePage)
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');

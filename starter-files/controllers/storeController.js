1bd81d19b4569fe0171734f156e1046d45c44195
dsanderson90 <dsanderson3@cnm.edu>
2019-05-08 08:43:48 -0600

    got store data to render on homepage and /stores url and created template + mixin to render it

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  console.log(req.message);
   res.render('index');
 };
 
@@ -14,7 +12,14 @@ exports.addStore = (req, res) => {
 
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();;
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //  1. Query the database for a list of all stores
+  const stores = await Store.find();
+  // 2. Pass data to the template
+  res.render('stores', { title: 'Stores', stores});
 };
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -7,8 +7,11 @@ const { catchErrors } = require('../handlers/errorHandlers')
 //Pug is a popular html templating language that used to be called Jade. Its often used by Node developers
 
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
+// since the createStore method uses async await we have to wrap it with a higher order function which catches any errors if the post is unsucessfull
+// this way we don't have to add try catch blogs to each controller method. 
 router.post('/add', catchErrors(storeController.createStore));
 
 router.get('/reverse/:name', (req, res) => {

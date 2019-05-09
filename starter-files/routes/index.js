840e54e1de36e29acd3ff53bebb3d599b2b41001
dsanderson90 <dsanderson3@cnm.edu>
2019-05-09 15:02:07 -0600

    initial commit to new repo

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -24,5 +24,7 @@ catchErrors(storeController.resize),
 catchErrors(storeController.updateStore));
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
+
 
 module.exports = router;
